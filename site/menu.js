8a755070a889857e588752b864f1373da5889103
Connor L <73072926+ConnorL26@users.noreply.github.com>
2025-08-13 07:37:01 +0100

    Delete site/menu.js

--- site/menu.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var sidebar = document.getElementById('sidebar');
-    var toggle = document.getElementById('sidebarToggle');
-    toggle.addEventListener('click', function () {
-        sidebar.classList.toggle('open');
-    });
-});
